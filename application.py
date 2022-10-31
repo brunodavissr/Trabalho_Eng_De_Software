ee3edd6cf28c4f246321b1e794bb9579570f2ca8
Bruno Davis <91693335+brunodavissr@users.noreply.github.com>
2022-10-30 22:05:07 -0300

    Create application.py

---
+++ application.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+times_cadastrados = []
+matriculas_cadastradas = []
+emails_cadastrados = []
+
+@app.route("/")
+def index():
+        return render_template("index.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    nome_time = request.form.get("nome_time")
+    matricula1 = request.form.get("primeiro_int")
+    matricula2 = request.form.get("segundo_int")
+    matricula3 = request.form.get("terceiro_int")
+    email_time = request.form.get("email_time")
+    nivel = request.form.get("nivel")
+
+    if nome_time in times_cadastrados:
+        return "Time já cadastrado. Escolha outro nome para seu time."
+    elif matricula1 in matriculas_cadastradas:
+        return str(matricula1) + " já participa de um time!"
+    elif matricula2 in matriculas_cadastradas:
+        return str(matricula2) + " já participa de um time!"
+    elif matricula3 in matriculas_cadastradas:
+        return str(matricula3) + " já participa de um time!"
+    elif matricula1 == matricula2 or matricula1 == matricula3 or matricula2 == matricula3:
+        return "Matrículas iguais. Não pode haver número de matrículas iguais no mesmo time."
+    elif email_time in emails_cadastrados:
+        return "Email já cadastrado em outro time. Defina outro e-mail para seu time"
+    else:
+        times_cadastrados.append(nome_time)
+        matriculas_cadastradas.append(matricula1)
+        matriculas_cadastradas.append(matricula2)
+        matriculas_cadastradas.append(matricula3)
+        emails_cadastrados.append(email_time)
+        return "Time " + nome_time + " cadastrado com sucesso no nivel " + nivel
