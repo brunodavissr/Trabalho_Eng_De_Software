8efa075b76dbcd51466a9091c4b0f298ff453e92
Bruno Davis <91693335+brunodavissr@users.noreply.github.com>
2022-11-02 18:09:29 -0300

    Delete static directory

--- static/script.js
+++
@@ -1,7 +0,0 @@
-let resultado = document.getElementById("resultado").innerText;
-console.log(resultado);
-
-if(resultado != "") {
-    var heightPage = document.body.scrollHeight;
-    window.scrollTo(0 , heightPage);
-}
